4a5db178535d26051b46a854ff1ae1231ce1f331
Sümegi Zoltán <sumegizoltan73@gmail.com>
2021-09-22 13:32:50 +0200

    JSX működése

--- my-react/src/App.js
+++ my-react/src/App.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Sziasztok!</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    /*
+    return (
+      <div className="App">
+        <h1>Sziasztok!</h1>
+      </div>
+    );
+    */
+    return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Sziasztok!'));
+  } 
 }
 
 export default App;
