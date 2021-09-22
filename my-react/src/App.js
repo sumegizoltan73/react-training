3753c03449714e490d9c618a1e8f52cd3aad7c6e
Sümegi Zoltán <sumegizoltan73@gmail.com>
2021-09-22 14:01:18 +0200

    A state tulajdonság

--- my-react/src/App.js
+++ my-react/src/App.js
@@ -3,15 +3,23 @@ import Person from './Person/Person';
 import './App.css';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Hűbele Balázs', age: 26 },
+      { name: 'János', age: 58 },
+      { name: 'Kocka Karesz', age: 20 }
+    ]
+  }
+
   render() {
     
     return (
       <div className="App">
         <h1>Sziasztok!</h1>
         <p>Bekezdés</p>
-        <Person name="Hűbele Balázs" age="26"/>
-        <Person name="János" age="58"/>
-        <Person />
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>Hobbim az informatika!</Person>
       </div>
     );
     
