2877520f7f1dee772908173dc6fe3772b1d8ebe9
Sümegi Zoltán <sumegizoltan73@gmail.com>
2021-09-22 13:40:10 +0200

    új komponens

--- my-react/src/App.js
+++ my-react/src/App.js
@@ -1,16 +1,19 @@
 import React, {Component} from 'react';
+import Person from './Person/Person'; 
 import './App.css';
 
 class App extends Component {
   render() {
-    /*
+    
     return (
       <div className="App">
         <h1>Sziasztok!</h1>
+        <p>Bekezdés</p>
+        <Person />
       </div>
     );
-    */
-    return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Sziasztok!'));
+    
+    //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Sziasztok!'));
   } 
 }
 
