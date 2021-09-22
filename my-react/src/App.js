a9d601afabc9cba56d88293ecbd7d8f770eee61b
Sümegi Zoltán <sumegizoltan73@gmail.com>
2021-09-22 15:31:41 +0200

    Komponensek listázása a map methodussal

--- my-react/src/App.js
+++ my-react/src/App.js
@@ -56,25 +56,14 @@ class App extends Component {
     if (this.state.lathatosag) {
       persons = (
         <div>
-          <Person 
-            name={this.state.persons[0].name} 
-            age={this.state.persons[0].age}
-            click={this.nameChangeHandler.bind(this, 'Hübele Bazsi')}
-            change={this.nevValtozasKezelo}
-          />
-          <Person 
-            name={this.state.persons[1].name} 
-            age={this.state.persons[1].age}
-            click={(event) => this.nameChangeHandler('Csak Balázs')}
-            change={this.nevValtozasKezelo}
-          />
-          <Person 
-            name={this.state.persons[2].name} 
-            age={this.state.persons[2].age}
-            change={this.nevValtozasKezelo}
-          >
-              Hobbim az informatika!
-          </Person>
+          {
+            this.state.persons.map(person => {
+              return(
+                <Person name={person.name} age={person.age} />
+              )
+            })
+          }
+          
         </div>
       );
     }
